5d21d90dceab3f41eb3cb2dda362829e042843dd
Gustavo H. Pulner <contami732@gmail.com>
2022-01-27 20:51:12 -0300

    Correção de erros
    Correção dos erros na lista de JS

--- JS/Lista01/Ex06.js
+++ JS/Lista01/Ex06.js
@@ -1,14 +1,21 @@
-var maior = -999999999, menor = 999999999, numero
+var maior = -999999999, menor = 999999999, numero, verificador = false
 
 for (var cont = 1; cont <= 5; cont++) {
     numero = parseFloat(prompt("Olá, digite um numero: "))
 
     if (numero >= maior) {
         maior = numero
+        verificador = true
     }
     if (numero <= menor) {
         menor = numero
+        verificador = true
     }
 }
-alert("O maior numero digitado é o: " + maior)
-alert("O menor numero digitado é o: " + menor)
+
+if (verificador == true) {
+    alert("O maior numero digitado é o: " + maior)
+    alert("O menor numero digitado é o: " + menor)
+} else {
+    alert("Nenhum numero foi digitado")
+}
